dcef4eb468552b693cbd01030b05ed6d0c3f2c14
polindinh <polin_dinh88@web-posting.us-central1-a.c.sunlit-plasma-264816.internal>
2020-04-19 14:39:07 +0000

    added Like button

---
+++ Tweeter/resources/js/components/LikeButton.vue
@@ -0,0 +1,30 @@
+
+<template>
+    <a class="level-item button is-small is-info" @click="like" >
+        <span class="icon is-small"><i class="fas fa-thumbs-up"></i></span>
+        <span>Like</span>
+    </a>
+</template>
+
+<script>
+export default {
+    name: "TweetLikeButton",
+    props: {
+        tweetId: "",
+    },
+    methods: {
+        like(){
+            axios.post('/tweet/like',{
+                tweetId: this.tweetId,
+            })
+            .then(response => {
+                this.$root.$emit('tweetLiked', this.tweetId);
+                console.log("liked");
+            });
+        },
+    },
+}
+</script>
+
+<style>
+</style>
